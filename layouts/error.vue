1039fa63ce7bef24de2ebb4b69d0ccf2d0a1db81
Firzan Azrai <firzanazrai97@gmail.com>
2021-08-29 08:05:16 +0800

    add 404 page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="h-5/6 flex items-center justify-center">
-    <div class="my-3 flex md:flex-row flex-col items-start justify-center">
+    <div
+      class="
+        my-3
+        flex
+        md:flex-row
+        flex-col
+        md:items-start
+        items-center
+        justify-center
+      "
+    >
+      <!-- Status code -->
       <div
         class="
           text-red-500 text-6xl
@@ -12,32 +23,35 @@
       >
         404
       </div>
-      <div>
+      <div class="md:text-left text-center">
+        <!-- Title -->
         <div class="text-white text-6xl font-bold">Page not found</div>
+        <!-- Subtitle -->
         <div class="text-gray-300 py-2">
           Please check the URL in the address bar and try again
         </div>
-        <div>
-          <nuxt-link tag="div" to="/">
-            <div
-              class="
-                p-2
-                text-center
-                w-1/3
-                bg-red-200
-                rounded-sm
-                shadow-lg
-                hover:bg-red-500 hover:text-white
-                transition
-                duration-700
-                cursor-pointer
-                font-medium
-              "
-            >
-              Go back home
-            </div></nuxt-link
+        <!-- Home button -->
+        <nuxt-link tag="div" to="/">
+          <div
+            class="
+              p-2
+              text-center
+              w-1/3
+              md:ml-0
+              mx-auto
+              bg-red-200
+              rounded-sm
+              shadow-lg
+              hover:bg-red-500 hover:text-white
+              transition
+              duration-700
+              cursor-pointer
+              font-medium
+            "
           >
-        </div>
+            Home
+          </div></nuxt-link
+        >
       </div>
     </div>
   </div>
@@ -48,5 +62,3 @@ export default {
   layout: 'none',
 }
 </script>
-
-<style></style>
